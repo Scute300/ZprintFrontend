e84d2fbed1c5eb1ee761e95775d2f9649568731b
antonio <antoniocelaya@scuteapp.com>
2021-03-06 12:16:01 -0600

    "initial commit"

---
+++ pages/Dashboard/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns">
+    <div class="column is-narrow">
+        <button class="button is-primary">
+          <a href="Dashboard">
+            <span>
+              Empezar
+            </span>
+          </a>
+        </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-narrow">
+          <button class="button is-primary">
+            <a href="Dashboard">
+              <span>
+                Empezar
+              </span>
+            </a>
+          </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+</style>
