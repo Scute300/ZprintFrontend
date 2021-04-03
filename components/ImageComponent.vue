624d9648d69698dee61b6c03e98f065bfa449f4b
antonio <antoniocelaya@scuteapp.com>
2021-04-03 11:15:24 -0600

    "Form for add item"

---
+++ components/ImageComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="columns is-centered">
+      <div class="column is-narrow">
+        <figure class="image is-256x256">
+          <img src="https://bulma.io/images/placeholders/128x128.png">
+        </figure>
+      </div>
+    </div>
+    <div class="columns is-centered">
+      <div class="column is-narrow">
+        <div class="file is-normal has-name"  >
+          <label class="file-label">
+            <input class="file-input" type="file" name="resume"/>
+            <span class="file-cta">
+              <span class="file-icon">
+                <i class="fas fa-upload" />
+              </span>
+              <span class="file-label">
+                name file
+              </span> 
+            </span>
+            <span class="file-name">
+               28/08/21
+            </span> 
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ImageComponent',
+  components: {
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    GoRoute(param) {
+      this.$router.push(param)
+    },
+  },
+})
+</script>
+
+<style>
+</style>
--- pages/Inventario/Detail/:action/:id.vue
+++ pages/Inventario/Detail/:action/:id.vue
@@ -3,10 +3,126 @@
     <Navbar />
     <div class="container">
       <HeaderDetailScreen />
-      <div class="column is-centered">
-        <div class="column is-narrow">
+      <div class="columns">
+        <div class="column is-12">
           <div class="box">
+            <ImageComponent />
           </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-7">
+          <form>
+            <div class="field has-addons">
+              <div class="control">
+                <input class="input" type="text" placeholder="Escanear Codigo de barras" />
+              </div>
+              <div class="control">
+                <button class="button is-success">
+                  Escanear
+                </button>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">
+                Nombre*
+              </label>
+              <div class="control">
+                <input class="input is-fullwidth" type="text" placeholder="Nombre del producto">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">
+                Precio*
+              </label>
+              <div class="control">
+                <input class="input is-fullwidth" type="number" placeholder="Precio del producto">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">
+                Precio 2
+              </label>
+              <div class="control">
+                <input class="input is-fullwidth" type="number" placeholder="Precio del producto">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">
+                  Costo
+              </label>
+              <div class="control">
+                <input class="input is-fullwidth" type="number" placeholder="Precio de compra">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">
+                  Existencias
+              </label>
+              <div class="control">
+                <input class="input is-fullwidth" type="number" placeholder="Precio de compra">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-12">
+          <div class="box">
+            <div class="columns">
+              <div class="column is-7">
+                <form>
+                  <h3 class="title is-4">
+                    Información adicional  
+                  </h3>
+                  <div class="field">
+                    <div class="label">
+                      Marca
+                    </div>
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select>
+                          <option>Select dropdown</option>
+                          <option>With options</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="field">
+                    <div class="label is-fullwidth">
+                      Departamento
+                    </div>
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select>
+                          <option>Select dropdown</option>
+                          <option>With options</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="field">
+                    <div class="label is-fullwidth">
+                      Especificaciones
+                    </div>
+                    <div class="control">
+                      <textarea class="textarea is-fullwidth has-fixed-size" /> 
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <button class="button is-success">
+            Listo
+          </button>
+          <button class="button is-danger">
+            Cancelar
+          </button>
         </div>
       </div>
     </div>
@@ -17,10 +133,12 @@
 import Vue from 'vue'
 import Navbar from '../../../../components/Navbar'
 import HeaderDetailScreen from '../../../../components/HeaderDetailsScreen'
+import ImageComponent from '../../../../components/ImageComponent'
 export default Vue.extend({
   components: {
     Navbar,
     HeaderDetailScreen,
+    ImageComponent
   },
   data() {
     return {}
