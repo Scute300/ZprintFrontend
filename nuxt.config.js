59b5d9ada78d58e650b02b9ff122584a202cd0f9
antonio <antoniocelaya@scuteapp.com>
2021-04-19 01:16:55 -0500

    ruta

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
     scss: ['./styles/main.scss'],
   },
   axios: {
-    baseURL: 'http://localhost:3333/',
+    baseURL: 'https://zprintback.herokuapp.com/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
