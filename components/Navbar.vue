260bb577a10d672e7762f8305a93c20a9d44a0ff
antonioce <antoniocegu@gmail.com>
2021-07-12 10:02:13 -0500

    Edit and navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,39 @@
 <template>
   <div id="menu2">
+    <div v-if="blockScreen"  class="blackScreen" />
     <nav class="navbar is-fixed-top is-primary navTop" role="navigation" aria-label="dropdown navigation">
       <div class="navbar-brand">
-        <span class="navbar-item">
-          Z Print
+        <a 
+          @click="displayMenu"
+          class="navbar-item"
+        >
+          <i class="fas fa-bars fa-2x"></i>
+        </a>
+        <span class="navbar-item title is-4">
+          MAMMON
         </span>
       </div>
     </nav>
-    <div v-if="showMenu == true" id="menu">
-      <div class="container">
-        <div v-for="menu in menus" class="columns is-vcentered">
-          <div class="column is-12" id="menuContainer">
-            <a :href="menu.route">
-            <span class="icon-text">
-              <span class="icon is-large">
-                <i id="icon" :class="menu.icon"/>
-              </span>
-            </span>
-            </a>
+    <transition name="menu-element">
+      <div v-if="showMenu == true" id="menu">
+        <div class="container">
+          <div v-for="menu in menus" class="columns is-vcentered"> 
+            <div class="column is-12" id="menuContainer">
+              <a @click="goLink(menu.route)" style="display: flex; align-items: center;">
+                <span class="icon-text">
+                  <span class="icon is-large">
+                    <i id="icon" :class="menu.icon"/>
+                  </span>
+                </span>
+                <span class="title is-4 has-text-white">
+                  {{menu.name}}
+                </span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>  
   </div>
 </template>
 
@@ -32,7 +44,8 @@ export default Vue.extend({
   name: 'Navbar',
   data(){
     return{
-      showMenu: true,
+      showMenu: false,
+      blockScreen: false,
       menus: [
         {name: 'User', icon: 'fas fa-user fa-2x'},
         {name: 'Escritorio', icon: 'fas fa-chart-line fa-2x', route: '/Dashboard'},
@@ -40,14 +53,33 @@ export default Vue.extend({
         {name: 'Principal', icon:'fas fa-th-large fa-2x', route: '/Main'},
         {name: 'Inventario', icon: 'fas fa-clipboard-list fa-2x', route: '/Inventario'},
         {name: 'Reportes', icon: 'fas fa-chart-pie fa-2x'},
-        {name: 'Configuraciones', icon: 'fas fa-cog fa-2x'},
+        {name: 'Configuraciones', icon: 'fas fa-cog fa-2x', route: '/Config'},
         {name: 'Suscripción', icon:'fas fa-project-diagram fa-2x', route: '/Payment'}
       ]
     }
   },
   methods:{
     goLink(param){
-      this.$router.push(param)
+      this.displayMenu()
+      setTimeout(() => {
+        this.$router.push(param)
+      }, 1000)
+    },
+    displayMenu(){
+      if (this.showMenu) {
+        this.showMenu = false
+        setTimeout(() => {
+          if (!this.showMenu) {
+           this.blockScreen = false
+   
+          }
+        }, 1000)
+      } else {
+        this.blockScreen = true
+        if (this.blockScreen) {
+          this.showMenu = true
+        }
+      }
     }
   }
 
@@ -60,8 +92,8 @@ export default Vue.extend({
 }
 #menu{
   background-color: #00D1B2;
-  width: 50px;
-  height: 92.5vh;
+  width: 300px;
+  height: 95vh;
   position:absolute;
   z-index: 3;
   position: fixed;
@@ -78,5 +110,29 @@ export default Vue.extend({
 }
 #icon{
   color: white;
+}
+.blackScreen{
+    display: flex;
+    flex: 1;
+    top: 0px;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: .8;
+    z-index: 2;
+}
+.menu-element-enter-active {
+  transition: all 1s ease;
+}
+
+.menu-element-leave-active {
+  transition: all 1s ease
+}
+
+.menu-element-enter, .menu-element-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(-300px);
+  opacity: 0;
 }
 </style>
---
+++ pages/Config/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <div class="columns">
+        <div v-for="menu in menus" class="column is-3">
+          <a v-on:click="GoRoute(menu.route)">
+            <div class="box" :style="{ backgroundColor: menu.color, display: 'flex', justifyContent: 'center' }">
+              <figure>
+                <img v-bind:src="menu.icon" width="100" height="100"/>
+              </figure>
+            </div>
+          </a>
+          <span class="has-text-link has-text-centered" id="mySpan">
+            {{ menu.name }}
+          </span>
+        </div>     
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from '../../components/Navbar.vue'
+  export default{
+    components: {
+      Navbar
+    },
+    data(){
+      return{
+        menus: [
+          {
+            name: 'Perfil',
+            icon: require('../../assets/icons/014-name.png'),
+            route: '/Gastos/nuevo',
+            color: '#9CD77D',
+          },
+          {
+            name: 'Usuarios',
+            icon: require('../../assets/icons/017-user.png'),
+            route: '/Prestamos',
+            color: '#C6D18F',
+          },
+          {
+            name: 'Ticket',
+            icon: require('../../assets/icons/bill.png'),
+            route: '/Prestamos',
+            color: '#8878B9',
+          },
+        ]
+      }
+    },
+    methods: {
+      GoRoute(param) {
+        this.$router.push(param)
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
+
